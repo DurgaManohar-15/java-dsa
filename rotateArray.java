7b937849e3f5576e782ee8b5206caa34f00403f6
DurgaManohar-15 <durgamanohat@gamil.com>
2025-05-17 10:16:04 +0530

    second commit

--- rotateArray.java
+++ rotateArray.java
@@ -1,9 +1,10 @@
 public class rotateArray {
    public static void main(String[] args) {
-    int arr[] = {1, 2, 3};
+    int arr[] = {1, 2, 3, 4, 5, 6};
     int n =arr.length;
-    int d =4;
+    int d =2;
     int count =0 ; 
+
 /*     int temp[] = new int [ n];
 
     for(int i=d;i<n;i++){
@@ -24,7 +25,7 @@ for(int i=0;i<d;i++){
         }
 
          for(int j=0;j<arr.length;j++){
-            System.out.print(arr[j]);
+            System.out.print(arr[j]+" ");
          } 
 
 
